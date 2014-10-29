ed739dad13a6b3d0ed1ae13d184aba0cdc1f8545
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-10-29 00:00:32 -0700

    All tests passing

--- test/authorization.js
+++ test/authorization.js
@@ -47,117 +47,164 @@ describe('Authorization Validation', function(){
         utilities.runMiddlewares(middlewares, req);
     });
 
-    xit('should return an object representing the authorization method supplied.', function(done) {
-        var options = {
-                host: 'localhost',
-                port: port,
-                path: '/',
-                method: 'GET',
-                headers: {
+    it('should attach the auth method to req.bridgetown.method', function(done) {
+        var req = {
+                headers : {
                     authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
                 }
-            };
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(),
+                function(req, res, next) {
+                    req.bridgetown.method.should.equal('Basic');
+                    next();
+                },
+                done.bind(done, undefined)
+            ];
 
-        server.start(function(req, res) {
-            middleware.authorization(req, res, function() {
-                var response = new Response(res);
-                req.bridgetown.method.should.equal('Basic');
-                req.bridgetown.token.should.equal('Aladdin:open sesame');
-                response.write(200, {success:true});
-            });
-        });
-
-        request(options)
-            .then(function(response) {
-                response.success.should.equal(true);
-                done();
-            })
-            .catch(done);
+        utilities.runMiddlewares(middlewares, req);
     });
 
-    xit('should return an Google as the authentication method when given google in the token.', function(done) {
-        var options = {
-                host: 'localhost',
-                port: port,
-                path: '/',
-                method: 'GET',
-                headers: {
+    it('should attach the decoded token to req.bridgetown.token', function(done) {
+        var req = {
+                headers : {
+                    authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
+                }
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(),
+                function(req, res, next) {
+                    req.bridgetown.token.should.equal('Aladdin:open sesame');
+                    next();
+                },
+                done.bind(done, undefined)
+            ];
+
+        utilities.runMiddlewares(middlewares, req);
+    });
+
+    it('should attach Google as the authentication method when given google in the token.', function(done) {
+        var req = {
+                headers : {
                     authorization: 'Google QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
                 }
-            };
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(),
+                function(req, res, next) {
+                    req.bridgetown.method.should.equal('Google');
+                    next();
+                },
+                done.bind(done, undefined)
+            ];
 
-        server.start(function(req, res) {
-            middleware.authorization(req, res, function(){
-                var response = new Response(res);
-                req.bridgetown.method.should.equal('Google');
-                req.bridgetown.token.should.equal('Aladdin:open sesame');
-                response.write(200, {success:true});
-            });
-        });
-
-        request(options)
-            .then(function(response) {
-                response.success.should.equal(true);
-                done();
-            })
-            .catch(done);
+        utilities.runMiddlewares(middlewares, req);
     });
 
-    xit('should return an Google as the authentication method when given google in the token and 2 spaces between', function(done) { //jshint ignore:line
-        var options = {
-                host: 'localhost',
-                port: port,
-                path: '/',
-                method: 'GET',
-                headers: {
+    it('should attach Google as the authentication method when given google in the token - even with 2 spaces for separation.', function(done) {
+        var req = {
+                headers : {
                     authorization: 'Google  QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
                 }
-            };
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(),
+                function(req, res, next) {
+                    req.bridgetown.method.should.equal('Google');
+                    next();
+                },
+                done.bind(done, undefined)
+            ];
 
-        server.start(function (req, res) {
-            middleware.authorization(req, res, function() {
-                var response = new Response(res);
-                req.bridgetown.method.should.equal('Google');
-                req.bridgetown.token.should.equal('Aladdin:open sesame');
-                response.write(200, {success:true});
-            });
-        });
-
-        request(options)
-            .then(function(response) {
-                response.success.should.equal(true);
-                done();
-            })
-            .catch(done);
+        utilities.runMiddlewares(middlewares, req);
     });
 
-    xit('Should have a default method of Basic, when only a token is passed.', function(done) {
-        var options = {
-                host: 'localhost',
-                port: port,
-                path: '/',
-                method: 'GET',
-                headers: {
+    it('Should have a default method of Basic, when only a token is passed.', function(done) {
+        var req = {
+                headers : {
                     authorization: 'QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
                 }
-            };
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(),
+                function(req, res, next) {
+                    req.bridgetown.token.should.equal('Aladdin:open sesame');
+                    next();
+                },
+                done.bind(done, undefined)
+            ];
 
-        server.stop();
-        server.start(function(req, res) {
-            middleware.authorization(req, res, function() {
-                var response = new Response(res);
-                req.bridgetown.method.should.equal('Basic');
-                response.write(200,{success:true});
-            });
-        }).then(function(){
-            request(options)
-                .then(function(response) {
-                    response.success.should.equal(true);
-                    done();
-                })
-                .catch(done);
-        });
-
+        utilities.runMiddlewares(middlewares, req);
     });
 
+    it('should set req.bridgetown.identity to what is used to resolve the deferred passed to the auth method', function(done) {
+        var req = {
+                headers : {
+                    authorization: 'QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
+                }
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(function(tokenObject, deferred) {
+                    deferred.resolve('boomy');
+                }),
+                function(req, res, next) {
+                    req.bridgetown.identity.should.equal('boomy');
+                    next();
+                },
+                done.bind(done, undefined)
+            ];
+
+        utilities.runMiddlewares(middlewares, req);
+    });
+
+    it('should call the auth method with a token object made of the auth method and decoded auth token', function(done) {
+        var req = {
+                headers : {
+                    authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
+                }
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(function(tokenObj) {
+                    tokenObj.method.should.equal('Basic');
+                    tokenObj.token.should.equal('Aladdin:open sesame');
+                    done();
+                })
+            ];
+
+        utilities.runMiddlewares(middlewares, req);
+    });
+
+    it('rejecting the deferred passed into the auth method results in an unauthorized response', function(done) {
+        var req = {
+                headers : {
+                    authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
+                }
+            },
+            middlewares = [
+                bridgetown.middleware.initialize(),
+                bridgetown.middleware.authorization(function(tokenObj, deferred) {
+                    deferred.reject();
+                })
+            ],
+            options = utilities.runMiddlewares(middlewares, req);
+
+        process.nextTick(function() {
+            var res = options.res;
+            res.writeHead.should.have.been.calledWith(401, {'Content-Type': 'application/json'});
+            res.write.should.have.been.calledWith(JSON.stringify({
+                code: 401,
+                status: 'error',
+                message: 'Unauthorized'
+            }));
+            res.end.should.have.been.calledOnce;
+            done();
+        });
+    });
 });
